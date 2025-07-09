074fcf49db637e2a440865c34831ea97adaae1b2
Kamal Dhakad <kamal.d@mizzle.io>
2025-07-09 15:08:03 +0530

    fix: first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js
+const express = require("express");
+const Stripe = require("stripe");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const stripeKey = "";
+const stripe = Stripe(stripeKey);
+console.log(stripe, "stripeeeee");
+app.use(cors());
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const { amount, currency = "usd" } = req.body;
+
+    // Create a PaymentIntent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount, // in cents: e.g., 500 = $5.00
+      currency,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Stripe backend running on port ${PORT}`));
